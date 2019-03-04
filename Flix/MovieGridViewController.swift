94b168ee31433fab643777fb07e2524da0644a1e
Rafael Carmilema <rafaelcarmilema@gmail.com>
2019-03-04 00:21:05 -0500

    Fixed issues with images

--- Flix/MovieGridViewController.swift
+++ Flix/MovieGridViewController.swift
@@ -19,6 +19,12 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
         collectionView.delegate = self
         collectionView.dataSource = self
         
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        
+        layout.minimumLineSpacing = 4
+        layout.minimumInteritemSpacing = 4
+        let width = (view.frame.size.width - layout.minimumInteritemSpacing * 2) / 3 //gets width of the phone divided by number of cells per row
+        layout.itemSize = CGSize(width: width, height: width * 3 / 2)
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
